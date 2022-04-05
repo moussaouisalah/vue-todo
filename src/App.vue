1d5551c12da9f252005bff543e9d66b03ceae74e
Salah Moussaoui <60707540+moussaouisalah@users.noreply.github.com>
2022-04-05 12:14:15 +0000

    add boilerplate for todo item

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <template>
   <h1>hello world!</h1>
+  <TodoItem v-for="item in todos" :key="item.id" :todo="item" />
 </template>
 
 <script>
+import TodoItem from "./components/TodoItem.vue";
+
 export default {
   name: "App",
+  components: {
+    TodoItem,
+  },
+  data() {
+    return {
+      todos: [
+        { id: 1, title: "Todo 1" },
+        { id: 2, title: "Todo 2" },
+        { id: 3, title: "Todo 3" },
+      ],
+    };
+  },
 };
 </script>
 
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    {{ todo.title }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+};
+</script>
+<style></style>
