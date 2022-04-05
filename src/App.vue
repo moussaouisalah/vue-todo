69a515cf369c637de78441bcaa2cf835d3c28c72
Salah Moussaoui <60707540+moussaouisalah@users.noreply.github.com>
2022-04-05 15:06:54 +0000

    add todo list filters

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,31 @@
     <AddTodo @add-todo="addTodo" />
     <div class="w-full flex flex-col items-center gap-2 max-w-xl">
       <h2 class="self-start text-gray-50 font-bold text-lg">Todos</h2>
-      <TodoItem
-        v-for="item in todos"
-        :key="item.id"
-        :todo="item"
-        @delete-todo="deleteTodo(item.id)"
-        @toggle-todo-status="toggleTodoStatus(item.id)"
-      />
+      <template v-if="todos.length > 0">
+        <div class="flex gap-1 w-full">
+          <FilterButton :isSelected="filter == 'all'" @select="filter = 'all'"
+            >All</FilterButton
+          >
+          <FilterButton
+            :isSelected="filter == 'active'"
+            @select="filter = 'active'"
+            >Active</FilterButton
+          >
+          <FilterButton
+            :isSelected="filter == 'completed'"
+            @select="filter = 'completed'"
+            >Completed</FilterButton
+          >
+        </div>
+        <TodoItem
+          v-for="item in filteredTodos"
+          :key="item.id"
+          :todo="item"
+          @delete-todo="deleteTodo(item.id)"
+          @toggle-todo-status="toggleTodoStatus(item.id)"
+        />
+      </template>
+      <p v-else class="text-gray-300 text-center">No Todos</p>
     </div>
   </div>
 </template>
@@ -20,17 +38,31 @@
 <script>
 import TodoItem from "./components/TodoItem.vue";
 import AddTodo from "./components/AddTodo.vue";
+import FilterButton from "./components/FilterButton.vue";
+
+const filters = {
+  all: (todos) => todos,
+  active: (todos) => todos.filter((todo) => !todo.isCompleted),
+  completed: (todos) => todos.filter((todo) => todo.isCompleted),
+};
 
 export default {
   name: "App",
   components: {
     TodoItem,
     AddTodo,
+    FilterButton,
   },
   data() {
     return {
       todos: [],
+      filter: "all",
     };
+  },
+  computed: {
+    filteredTodos() {
+      return filters[this.filter](this.todos);
+    },
   },
   methods: {
     addTodo({ title, description }) {
---
+++ src/components/FilterButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button
+    @click="$emit('select')"
+    :class="[
+      'text-xs font-semibold px-2.5 py-0.5 rounded',
+      isSelected
+        ? 'bg-yellow-200 text-yellow-900'
+        : 'bg-indigo-200 text-indigo-900',
+    ]"
+  >
+    <slot />
+  </button>
+</template>
+<script>
+export default {
+  name: "FilterButton",
+  props: ["isSelected"],
+  emits: ["select"],
+};
+</script>
